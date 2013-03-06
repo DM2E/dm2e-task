222db7625013f34fca9dc2a89cb09f17bf5b542f
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-03-06 05:07:58 +0100

    Improvements on JobService * PUT method for updating jobs * Use RabbitConnection and MongoConnection instead of that hacky ABC * Use CatchallJerseyExceptions when something goes wrong * Send resource URL to RabbitMQ queue (jobQueue) after POSTing jobs

--- src/main/java/eu/dm2e/task/services/JobService.java
+++ src/main/java/eu/dm2e/task/services/JobService.java
@@ -8,13 +8,12 @@ package eu.dm2e.task.services;
  * 		Body contains URL of a transformation specification which must be dereferenceable
  */
 
-import java.io.EOFException;
 import java.io.IOException;
-import java.util.logging.Logger;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -24,93 +23,128 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.UriInfo;
 
+import org.apache.log4j.Logger;
 import org.bson.types.ObjectId;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.jongo.Jongo;
-import org.jongo.MongoCollection;
 
 import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
-import com.mongodb.WriteResult;
 import com.mongodb.util.JSON;
+import com.rabbitmq.client.Channel;
 
 import eu.dm2e.task.model.Job;
 import eu.dm2e.task.model.JobStatus;
+import eu.dm2e.task.util.CatchallJerseyException;
+import eu.dm2e.task.util.MongoConnection;
+import eu.dm2e.task.util.RabbitConnection;
 
 @Path("/job")
-public class JobService extends AbstractTaskService {
+public class JobService {
 	
-	private static ObjectMapper mapper = new ObjectMapper();
+	private static final Logger logger = Logger.getLogger(Job.class);
 
-	@Override
-	protected String getRabbitQueueName() {
-		return "eu.dm2e.task.jobs";
-	}
-	
 	@GET
 	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getJob(@PathParam("id") String id) throws IOException {
-		Logger log = Logger.getLogger(getClass().getName());
+	public Response getJob(@PathParam("id") String id) throws CatchallJerseyException {
 		Job retrievedJob;
-		
-		DB mongo  = getMongoConnection();
-		DBCollection coll = mongo.getCollection("jobs");
-//	    MongoCollection jobs = jongo.getCollection("jobs");
+
+		logger.warn("I LIVE?");
+		DBCollection jobs = MongoConnection.getCollection("jobs");
 		try {
-			DBObject doc = coll.findOne(new BasicDBObject("_id", new ObjectId(id)));
+			DBObject doc = jobs.findOne(new BasicDBObject("_id", new ObjectId( id)));
 			retrievedJob = Job.fromMongoDoc(doc);
-			log.warning(retrievedJob.toString()	);
 		} catch (IllegalArgumentException e) {
-			return Response.status(400).entity("Error: Malformed job ID").build();
-		} catch (NullPointerException e) {
-			return Response.status(404).entity(e.toString()).build();
-		} finally {
-//			closeMongoConnection(jongo);
+			throw new CatchallJerseyException(e, "Malformed job ID");
+//		} catch (Exception e) {
+//			throw new CatchallJerseyException(e);
 		}
-		
+
 		ResponseBuilder builder;
 		if (retrievedJob.getStatus() == JobStatus.FINISHED) {
 			builder = Response.status(200);
-		}
-		else if (retrievedJob.getStatus() == JobStatus.NOT_STARTED) {
+		} else if (retrievedJob.getStatus() == JobStatus.NOT_STARTED) {
 			builder = Response.status(202);
+		} else {
+			builder = Response.ok(201);
 		}
-		else {
-			builder = Response.ok(202);
-		}
-		builder.entity(mapper.writeValueAsBytes(retrievedJob));
+		
+		builder.entity(retrievedJob.toJsonStr());
+		
 		return builder.build();
 	}
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response post(@Context UriInfo uriInfo, String body) throws IOException {
-//		Logger log  = Logger.getLogger(getClass().getName());
-		
+	public Response newJob(@Context UriInfo uriInfo, String body) throws CatchallJerseyException {
+
+		Job.validateJsonJob(body);
 		DBObject bodyDoc = (DBObject) JSON.parse(body);
-//		log.warning(bodyDoc.toString());
+
+		DBCollection jobs = MongoConnection.getCollection("jobs");
+
+		// first save to get an _id
+		jobs.save(bodyDoc);
+
+		// build Job POJO
+		Job job = Job.fromMongoDoc(bodyDoc);
 		
-	    DB mongo = getMongoConnection();
-	    DBCollection jobs = mongo.getCollection("jobs");
-	    
-	    // first save to get an _id
-	    jobs.save(bodyDoc);
-	    
-	    // build Job POJO
-	    Job job = Job.fromMongoDoc(bodyDoc);
-	    // set the url
-	    job.setJobURL(uriInfo.getRequestUri() + "/" + job.getJobID());
-	    // save again for the url
-	    jobs.save(job.toMongoDoc());
-	    
-	    // TODO post to worker via RabbitMQ!
-	    
-	    return Response.status(202).entity(job.toJsonStr()).build();
+		// set the url
+		job.setJobURL(uriInfo.getRequestUri() + "/" + job.get_id());
+		
+		// save again for the url
+		jobs.save(job.toMongoDoc());
+
+		// TODO post to worker via RabbitMQ!
+		byte[] messageBytes = job.getJobURL().getBytes();
+		Channel channel;
+		try {
+			channel = RabbitConnection.getChannel();
+			channel.basicPublish("", job.getJobQueue(), null, messageBytes);
+			channel.close();
+		} catch (IOException e) {
+			throw new CatchallJerseyException(e);
+		}
+		
+		return Response.status(202).entity(job.toJsonStr()).build();
+	}
+
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{id}")
+	public Response updateJob(@Context UriInfo uriInfo, @PathParam("id") String id, String body) {
+//		try {
+			
+			DBCollection jobs = MongoConnection.getCollection("jobs");
+			
+			// a document with just the id set, i.e. the query
+			BasicDBObject idDoc = new BasicDBObject().append("_id", new ObjectId(id));
+			
+			// the changed doc
+			DBObject doc = (DBObject) JSON.parse(body);
+			
+			// the doc that should be updated
+			DBObject docToUpdate = jobs.findOne(new BasicDBObject().append("_id", new ObjectId(id)));
+			
+			// update the doc
+			docToUpdate.putAll(doc);
+			
+			// and update it
+			jobs.update(idDoc, docToUpdate);
+			logger.warn(idDoc);
+			logger.warn(docToUpdate);
+			
+			DBObject savedDoc = jobs.findOne(idDoc);
+			logger.warn(savedDoc);
+					
+			// all went well
+			return Response.status(202).entity(docToUpdate.toString()).build();
+			
+//		} catch (Exception e) {
+//			throw new CatchallJerseyException(e);
+//		}
 	}
 
 }
