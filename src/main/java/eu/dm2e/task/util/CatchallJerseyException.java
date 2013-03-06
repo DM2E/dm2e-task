d0a2ec9551621c29c948a0a540343d9f4b60b8cd
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-03-06 03:39:06 +0100

    Basic Content Negotiation for the exception classes

--- src/main/java/eu/dm2e/task/util/CatchallJerseyException.java
+++ src/main/java/eu/dm2e/task/util/CatchallJerseyException.java
@@ -1,5 +1,8 @@
 package eu.dm2e.task.util;
 
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
 
 public class CatchallJerseyException extends Exception {
 	
@@ -18,12 +21,32 @@ public class CatchallJerseyException extends Exception {
 	public String getMessage() { return message; }
 	public void setMessage(String message) { this.message = message; }
 	
+	private int httpStatus = 400;
+	public int getHttpStatus() { return httpStatus; }
+	public void setHttpStatus(int httpStatus) { this.httpStatus = httpStatus; }
+	
 	public CatchallJerseyException(Exception e) {
 		this.setWrappedException(e);
 	}
 	public CatchallJerseyException(Exception e, String str) {
 		this.setWrappedException(e);
 		this.setMessage(str);
+	}
+	public CatchallJerseyException(Exception e, String str, int status) {
+		this.setWrappedException(e);
+		this.setMessage(str);
+		this.setHttpStatus(status);
+	}
+	public CatchallJerseyException(String str) {
+		Exception e = new Exception(str);
+		this.setWrappedException(e);
+		this.setMessage(str);
+	}
+	public CatchallJerseyException(String str, int status) {
+		Exception e = new Exception(str);
+		this.setWrappedException(e);
+		this.setMessage(str);
+		this.setHttpStatus(status);
 	}
 
 }
--- src/main/java/eu/dm2e/task/util/CatchallJerseyExceptionMapper.java
+++ src/main/java/eu/dm2e/task/util/CatchallJerseyExceptionMapper.java
@@ -1,10 +1,18 @@
 package eu.dm2e.task.util;
 
+import java.util.List;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.log4j.Logger;
+
+import com.mongodb.BasicDBObject;
 
 @Provider
 public class CatchallJerseyExceptionMapper implements
@@ -12,20 +20,48 @@ public class CatchallJerseyExceptionMapper implements
 
 	private static final Logger logger = Logger
 			.getLogger(CatchallJerseyExceptionMapper.class);
+	
+	// http://stackoverflow.com/questions/3227360/jax-rs-jersey-custom-exception-with-xml-or-json
+    @Context
+    private HttpHeaders headers;
 
-//	@Produces(MediaType.TEXT_PLAIN)
-	public Response toResponse(CatchallJerseyException e) {
-		logger.warn("hiya");
-		e.getWrappedException().printStackTrace();
-		return Response.status(Response.Status.BAD_REQUEST)
-				.header("Content-Type", "text/plain")
-				.entity( e.getWrappedException().getClass().getName()
-						+ "\n"
-						+ e.getWrappedException().toString() 
-						+ "\n"
-						+ e.getMessage()
-						+ "\n"
-					).build();
+	public Response toResponse( CatchallJerseyException e) {
+		String body = e.getWrappedException().toString();
+		String mediaType = "text/plain";
+		List<MediaType> acceptableList = headers.getAcceptableMediaTypes();
+		for (int i=0; i < acceptableList.size(); i++) {
+			String thisType = acceptableList.get(i).toString();
+			if (thisType.equals(MediaType.APPLICATION_JSON)) {
+				logger.warn("OI");
+				body = toResponse_APPLICATION_JSON(e);
+				mediaType = MediaType.APPLICATION_JSON;
+				break;
+			}
+		}
+		return Response
+				.status(e.getHttpStatus())
+				.header("Content-Type", mediaType)
+				.entity(body).build();
+	}
+	
+	public String toResponse_TEXT_PLAIN(CatchallJerseyException e) {
+		 return e.getWrappedException().getClass().getName()
+				 + "\n"
+				 + e.getMessage()
+				 + "\n"
+				 + e.getWrappedException().toString() 
+//				 + "\n"
+//				 + ExceptionUtils.getStackTrace(e)
+				 ;
+	}
+	public String toResponse_APPLICATION_JSON(CatchallJerseyException e) {
+		BasicDBObject doc = new BasicDBObject()
+			.append("name", e.getWrappedException().getClass().getName())
+			.append("message", e.getMessage())
+			.append("wrapped_message", e.getWrappedException().toString())
+//			.append("stacktrace", ExceptionUtils.getStackTrace(e.getWrappedException()))
+			;
+		return doc.toString();
 	}
 
 }
