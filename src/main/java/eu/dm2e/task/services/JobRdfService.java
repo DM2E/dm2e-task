1b109a91e9013e2ed8c1feca7b8a7e412dd44559
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-03-08 05:30:27 +0100

    Can now add and retrieve log entries from a job

--- src/main/java/eu/dm2e/task/services/JobRdfService.java
+++ src/main/java/eu/dm2e/task/services/JobRdfService.java
@@ -1,10 +1,8 @@
 package eu.dm2e.task.services;
 
 import java.io.File;
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.util.Date;
-import java.util.List;
 import java.util.logging.Logger;
 
 import javax.ws.rs.Consumes;
@@ -15,22 +13,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.PathSegment;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.NodeIterator;
-import com.hp.hpl.jena.rdf.model.Property;
-import com.hp.hpl.jena.update.UpdateExecutionFactory;
-import com.hp.hpl.jena.update.UpdateFactory;
-import com.hp.hpl.jena.update.UpdateProcessor;
-import com.hp.hpl.jena.update.UpdateRequest;
 
 import eu.dm2e.task.model.JobStatus;
 import eu.dm2e.task.util.CatchallJerseyException;
-import eu.dm2e.task.util.PATCH;
+import eu.dm2e.task.util.SparqlConstruct;
+import eu.dm2e.task.util.SparqlUpdate;
+import eu.dm2e.ws.grafeo.GLiteral;
 import eu.dm2e.ws.grafeo.GResource;
 import eu.dm2e.ws.grafeo.Grafeo;
 import eu.dm2e.ws.grafeo.jena.GrafeoImpl;
@@ -40,15 +33,20 @@ import eu.dm2e.ws.services.data.AbstractRDFService;
 public class JobRdfService extends AbstractRDFService {
 
 	private Logger log = Logger.getLogger(getClass().getName());
-	private static final String NS_DM2E = "http://onto.dm2e.eu/";
-	private static final String ENDPOINT = "http://lelystad.informatik.uni-mannheim.de:8080/openrdf-sesame/repositories/dm2etest";
-	private static final String ENDPOINT_STATEMENTS = "http://lelystad.informatik.uni-mannheim.de:8080/openrdf-sesame/repositories/dm2etest/statements";
-	private static final String JOB_STATUS_PROP = NS_DM2E + "status";
-	private static final String JOB_LOGENTRY_PROP = NS_DM2E + "logEntry";
-
+	private static final String 
+								NS_DM2E = "http://onto.dm2e.eu/",
+								NS_DM2E_LOGGING = "http://onto.dm2e.eu/logging#",
+								ENDPOINT = "http://lelystad.informatik.uni-mannheim.de:8080/openrdf-sesame/repositories/dm2etest",
+								ENDPOINT_STATEMENTS = "http://lelystad.informatik.uni-mannheim.de:8080/openrdf-sesame/repositories/dm2etest/statements",
+								JOB_STATUS_PROP = NS_DM2E + "status",
+								JOB_LOGENTRY_PROP = NS_DM2E + "hasLogEntry";
+	
+	private @Context UriInfo uriInfo;
+	
 	@GET
 	@Path("/{resourceID}")
-	public Response getResource(@Context UriInfo uriInfo,
+	@Consumes(MediaType.WILDCARD)
+	public Response getJob(@Context UriInfo uriInfo,
 			@PathParam("resourceID") String resourceID)
 			throws CatchallJerseyException {
 		// kb: need to use Jena model
@@ -107,6 +105,14 @@ public class JobRdfService extends AbstractRDFService {
 				.build();
 	}
 	
+	@PUT
+	@Consumes(MediaType.WILDCARD)
+	@Path("/{id}")
+	public Response replaceJob(@Context UriInfo uriInfo,
+			@PathParam("id") String id, String body) {
+		return null;
+	}
+	
 	public JobStatus getJobStatusInternal(String jobUriStr) throws CatchallJerseyException {
 		GrafeoImpl g = new GrafeoImpl();
 		g.readFromEndpoint(ENDPOINT, jobUriStr);
@@ -127,11 +133,13 @@ public class JobRdfService extends AbstractRDFService {
 		String resourceUriStr = uriInfo.getRequestUri().toString().replaceAll("/status$", "");
 		return Response.ok().entity(getJobStatusInternal(resourceUriStr).toString()).build();
 	}
-
+ 	
 	@PUT
 	@Consumes(MediaType.WILDCARD)
 	@Path("/{id}/status")
 	public Response updateJobStatus(@Context UriInfo uriInfo,
+
+			
 			@PathParam("id") String id, String newStatus)
 			throws CatchallJerseyException {
 		String resourceUriStr = uriInfo.getRequestUri().toString().replaceAll("/status$", "");
@@ -158,31 +166,76 @@ public class JobRdfService extends AbstractRDFService {
 		// replace the job status
 		String clauseDelete = String.format("<%s> <%s> ?old_status.", resourceUriStr, JOB_STATUS_PROP);
         String clauseInsert = String.format("<%s> <%s> \"%s\".", resourceUriStr, JOB_STATUS_PROP, newStatus);
-        StringBuilder sb;
-        sb = new StringBuilder();
-        sb.append("WITH <");
-        sb.append(resourceUriStr);
-		sb.append(">");
-        sb.append("DELETE { ");
-        sb.append(clauseDelete);
-        sb.append("} INSERT { ");
-        sb.append(clauseInsert);
-        sb.append("} WHERE { ");
-        sb.append(clauseDelete);
-        sb.append("}");
-        log.info("Modify query: " + sb.toString());
-        UpdateRequest update = UpdateFactory.create();
-        update.add(sb.toString());
-        UpdateProcessor exec = UpdateExecutionFactory.createRemoteForm(update, ENDPOINT_STATEMENTS);
-        exec.execute();
+        new SparqlUpdate.Builder()
+        	.graph(resourceUriStr)
+        	.delete(clauseDelete)
+        	.insert(clauseInsert)
+        	.where(clauseDelete)
+        	.endpoint(ENDPOINT_STATEMENTS)
+        	.build()
+	        .execute()
+        	;
+        	
+        // return the new status live
 		return getJobStatus(uriInfo, id);
 	}
 
-	@PUT
+	@POST
 	@Consumes(MediaType.WILDCARD)
-	@Path("/{id}")
-	public Response replaceJob(@Context UriInfo uriInfo,
-			@PathParam("id") String id, String body) {
-		return null;
+	@Path("/{id}/log")
+	public Response addLongEntry(@Context UriInfo uriInfo,
+			@PathParam("id") String id, 
+			File logRdfStr)
+			throws CatchallJerseyException {
+		
+		String resourceUriStr = uriInfo.getRequestUri().toString().replaceAll("/log$", "");
+		
+		Grafeo g = new GrafeoImpl();
+		long timestamp = new Date().getTime();
+		GLiteral timestampLiteral = g.date(timestamp);
+		log.info(new Long(timestamp).toString());
+		try {
+			g = new GrafeoImpl(logRdfStr);
+			// TODO validate if this is a valid log entry
+		} catch (Exception e) {
+			throw new CatchallJerseyException(e);
+		}
+		GResource blank = g.findTopBlank();
+		if (null == blank) throw new CatchallJerseyException("Must contain blank node");
+		String logEntryUriStr = uriInfo.getRequestUri().toString() + "/" + timestamp;
+		blank.rename(logEntryUriStr);
+		g.addTriple(resourceUriStr, "rdf:type", NS_DM2E_LOGGING + "LogEntry");
+		g.addTriple(resourceUriStr, JOB_LOGENTRY_PROP, logEntryUriStr);
+		g.addTriple(logEntryUriStr, NS_DM2E_LOGGING + "timestamp" , timestampLiteral);
+		g.writeToEndpoint(ENDPOINT_STATEMENTS, resourceUriStr);
+        	
+		return getResponse(g);
+//		return getJobStatus(uriInfo, id);
 	}
+//	
+	@GET
+	@Path("/{id}/log")
+	public Response listLogEntries() throws CatchallJerseyException {
+		
+		
+		String resourceUriStr = uriInfo.getRequestUri().toString().replaceAll("/log$", "");
+		SparqlConstruct sparqlConstruct;
+		Grafeo g;
+		try {
+			sparqlConstruct = new SparqlConstruct.Builder()
+					.endpoint(ENDPOINT)
+					.graph(resourceUriStr)
+					.construct("?s ?p ?o")
+					.where("?s a <" + NS_DM2E_LOGGING + "LogEntry> . ?s ?p ?o .")
+//					.where("?s a <" + NS_DM2E + "LogEntry>")
+					.build();
+			g = sparqlConstruct.execute();
+			
+		} catch (Exception e) {
+			throw new CatchallJerseyException(e);
+		}
+//		throw new CatchallJerseyException("TODO");
+		return getResponse(g);
+	}
+
 }
