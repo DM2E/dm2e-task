ae157dbe5ae6c9fe1b7bbdc22d6cd0b692908fe1
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-03-08 05:29:54 +0100

    some shell variables to make testing with curl easier

---
+++ bin/sh-curl-shortcuts.sh
@@ -0,0 +1,17 @@
+alias curl="eval /usr/bin/curl -v"
+POST="-X POST"
+GET="-X GET"
+PUT="-X PUT"
+DELETE="-X DELETE"
+
+CT_TURTLE='-H "Content-Type: text/turtle"'
+AC_TURTLE='-H "Accept: text/turtle"'
+
+CT_NTRIPLES='-H "Content-Type: text/plain"'
+AC_NTRIPLES='-H "Accept: text/plain"'
+
+CT_N3='-H "Content-Type: text/rdf+n3"'
+AC_N3='-H "Accept: text/rdf+n3"'
+
+CT_RDFXML='-H "Content-Type: application/rdf+xml"'
+AC_RDFXML='-H "Accept: application/rdf+xml"'
