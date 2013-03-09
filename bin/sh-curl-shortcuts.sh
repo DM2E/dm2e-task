e4c87a94619183bf53c9c522c85aa7aa9ca27adb
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-03-09 05:18:37 +0100

    fixed content types in bin/sh-curl-shortcuts.sh

--- bin/sh-curl-shortcuts.sh
+++ bin/sh-curl-shortcuts.sh
@@ -1,14 +1,18 @@
 alias curl="eval /usr/bin/curl -v"
-POST="-X POST"
+HEAD="-X HEAD"
 GET="-X GET"
+POST="-X POST"
 PUT="-X PUT"
 DELETE="-X DELETE"
+
+CT_TEXT='-H "Content-Type: text/plain"'
+AC_TEXT='-H "Accept: text/plain"'
 
 CT_TURTLE='-H "Content-Type: text/turtle"'
 AC_TURTLE='-H "Accept: text/turtle"'
 
-CT_NTRIPLES='-H "Content-Type: text/plain"'
-AC_NTRIPLES='-H "Accept: text/plain"'
+CT_NTRIPLES='-H "Content-Type: application/rdf-triples"'
+AC_NTRIPLES='-H "Accept: application/rdf-triples"'
 
 CT_N3='-H "Content-Type: text/rdf+n3"'
 AC_N3='-H "Accept: text/rdf+n3"'
